f25c4bdb8c50bc505c7a38b44b079bb00a7b6e40
James Daniels <jamesddaniels@gmail.com>
2017-02-09 10:12:03 -0600

    Created a class, basic program for storing grades. m2c2

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            GradeBook book = new GradeBook(); //constructor, new kw is invoking a new instance of GradeBook
+            book.AddGrade(91);
+            book.AddGrade(89.5f); //The f instructs to make this a float.
         }
     }
 
