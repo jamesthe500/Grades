c1f7b3dd02d8073b3d8ada3d1e17adda41ce5ba6
James Daniels <jamesddaniels@gmail.com>
2017-02-09 10:24:32 -0600

    Creates a custom constructor in GradeBook. m2c3

--- GradeBook.cs
+++ GradeBook.cs
@@ -8,11 +8,15 @@ namespace Grades
 {
     class GradeBook
     {
+        public GradeBook() // constructor, custom. Default (doesn't take parameters)
+        {
+            grades = new List<float>(); // This is brought from below. new instance of List with every instance of GradeBook.
+        }
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades  = new List<float>(); // must initialize a new instance of List, insure it points to an obj.
+        List<float> grades; //  = new List<float>(); // must initialize a new instance of List, insure it points to an obj.
     }
 }
